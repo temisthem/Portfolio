0bd34d737d4d576fb2af2b96467f81e16428641e
Karson Nguyen <karsonn@sfu.ca>
2025-01-22 20:02:39 -0800

    how about now?

---
+++ filePathPrefix.js
@@ -0,0 +1,6 @@
+﻿const isGitHubPages = window.location.hostname.includes("github.io");
+document.querySelectorAll("img").forEach(img => {
+    if (isGitHubPages && img.src.startsWith("images/")) {
+        img.src = "/public/" + img.src;
+    }
+});
