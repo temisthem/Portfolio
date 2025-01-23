c90110e7baaaf032f6519cb8ddada5a35a07f196
Karson Nguyen <karsonn@sfu.ca>
2025-01-22 17:40:53 -0800

    src to docs

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,5 +1,5 @@
 export default {
-  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+  content: ["./docs/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
     extend: {
       screens: {
--- vite.config.mjs
+++ vite.config.mjs
@@ -8,5 +8,5 @@ export default defineConfig({
     outDir: "../dist",
     emptyOutDir: true
   },
-  root: "src",
+  root: "docs",
 });
