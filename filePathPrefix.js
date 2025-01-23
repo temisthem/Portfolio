c020c084998efa4ee253f8536935117ade4a0989
Karson Nguyen <karsonn@sfu.ca>
2025-01-22 20:29:12 -0800

    surely this is ok

--- filePathPrefix.js
+++
@@ -1,8 +0,0 @@
-﻿const isGitHubPages = window.location.hostname.includes("github.io");
-document.querySelectorAll("img").forEach(img => {
-    if (isGitHubPages && img.src.startsWith("/images/")) {
-        img.src = "/public/" + img.src;
-        console.log("giving")
-    }
-});
-console.log('hi')
